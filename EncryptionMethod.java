bd9c77a583b435bf1927861b636d861dd797deef
DK <kinastowskid@gmail.com>
2020-10-24 11:49:29 +0200

    - Javadoc added for enum EncryptionMethod

--- EncryptionMethod.java
+++ EncryptionMethod.java
@@ -1,3 +1,14 @@
+
+/**
+ * Available encryption methods between
+ * client and server: 
+ * <p>
+ * {@link #NONE},
+ * {@link #XOR},
+ * {@link #CEZAR},
+ * 
+ * @author Dominik Kinastowski
+ */
 public enum EncryptionMethod {
     NONE("none"), XOR("xor"), CEZAR("cezar");
 
