30f8342a217c2d8c334dbbf40326dd941879cd08
DK <kinastowskid@gmail.com>
2020-10-24 13:50:44 +0200

    - first connection between Client and Server

--- Client.java
+++ Client.java
@@ -1,8 +1,43 @@
+import java.net.*;
+import java.io.*;
+
 public class Client {
-    public static void main(String[] args) {
-        
-        for(EncryptionMethod method: EncryptionMethod.values()){
-            System.out.println(method);
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public void startConnection(String ip, int port)  throws UnknownHostException, IOException{
+        this.clientSocket = new Socket(ip, port);
+        this.out = new PrintWriter(this.clientSocket.getOutputStream(), true);
+        this.in = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+    }
+
+    public String sendMessage(String msg) throws IOException {
+        this.out.println(msg);
+        String response = this.in.readLine();
+        return response;
+    }
+
+    public void stopConnection() {
+        try {
+			this.in.close();
+            this.out.close();
+            this.clientSocket.close();
+        } catch (IOException e) {
+			e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+	    Client client = new Client();
+	    try {
+			client.startConnection("127.0.0.1", 8080);
+            String response = client.sendMessage("hello from client");
+            System.out.println(response);
+        } catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+        };
     }
 }
--- Server.java
+++ Server.java
@@ -1,3 +1,35 @@
+import java.net.*;
+import java.io.*;
+
 public class Server {
-    
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public void start(int port) throws IOException {
+        this.serverSocket = new ServerSocket(port);
+        this.clientSocket = this.serverSocket.accept();
+        this.out = new PrintWriter(this.clientSocket.getOutputStream(), true);
+        this.in = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+        String greeting = this.in.readLine();
+        System.out.println(greeting);
+        out.println("hello form server");
+    }
+
+    public void stop() throws IOException {
+        this.in.close();
+        this.out.close();
+        this.clientSocket.close();
+        this.serverSocket.close();
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        try {
+            server.start(8080);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
