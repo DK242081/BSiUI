e30ffe2806d9177394e5af758a11c51541d97a34
DK <kinastowskid@gmail.com>
2020-10-25 22:54:22 +0100

    - Cesar support normal and capital letters

--- Encryptor.java
+++ Encryptor.java
@@ -11,6 +11,8 @@ public class Encryptor {
     }
 
     public String encrypt(String msg) {
+        this.key = 121411;
+        this.method = EncryptionMethod.CESAR;
         switch (this.method) {
             case NONE:
                 return msg;
@@ -45,10 +47,17 @@ public class Encryptor {
     }
 
     private String CesarEncrypt(String msg) {
-        System.out.println(this.key % 26);
         String encryptedMsg = "";
         for (int i = 0; i < msg.length(); i++) {
-            encryptedMsg = encryptedMsg + Character.toString((msg.charAt(i) - 65 + this.key) % 26 + 65);
+            if (msg.charAt(i) >= 65 && msg.charAt(i) < 91) {
+                encryptedMsg = encryptedMsg + Character.toString((msg.charAt(i) - 65 + this.key) % 26 + 65);
+                continue;
+            }
+            if (msg.charAt(i) >= 97 && msg.charAt(i) < 123) {
+                encryptedMsg = encryptedMsg + Character.toString((msg.charAt(i) - 97 + this.key) % 26 + 97);
+                continue;
+            }
+            encryptedMsg = encryptedMsg + Character.toString(msg.charAt(i));
         }
         return encryptedMsg;
     }
@@ -56,7 +65,15 @@ public class Encryptor {
     private String CesarDecrypt(String msg) {
         String decryptedMsg = "";
         for (int i = 0; i < msg.length(); i++) {
-            decryptedMsg = decryptedMsg + Character.toString((msg.charAt(i) - 65 + (26 - this.key % 26)) % 26 + 65);
+            if (msg.charAt(i) >= 65 && msg.charAt(i) < 91) {
+                decryptedMsg = decryptedMsg + Character.toString((msg.charAt(i) - 65 + (26 - this.key % 26)) % 26 + 65);
+                continue;
+            }
+            if (msg.charAt(i) >= 97 && msg.charAt(i) < 123) {
+                decryptedMsg = decryptedMsg + Character.toString((msg.charAt(i) - 97 + (26 - this.key % 26)) % 26 + 97);
+                continue;
+            }
+            decryptedMsg = decryptedMsg + Character.toString(msg.charAt(i));
         }
         return decryptedMsg;
     }
